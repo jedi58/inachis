15faa5c95d5459ae175c163ae7b5a82ddd6e19af
David G. Paul <jedi58@users.noreply.github.com>
2016-02-17 19:40:42 +0000

    Update install_phpcs.sh

--- build/install_phpcs.sh
+++ build/install_phpcs.sh
@@ -1,4 +1,2 @@
-curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
-mv phpcs.phar /usr/bin/phpcs
-curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
-mv phpcbf.phar /usr/bin/phpcbf
+curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar && chmod +x phpcs.phar && mv phpcs.phar /usr/bin/phpcs
+curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar && chmod +x phpcbf.phar && mv phpcbf.phar /usr/bin/phpcbf
