11fbfe080338c25f82b663d2ed7e74a667e328e1
David G. Paul <jedi58@gmail.com>
2016-02-17 19:38:13 +0000

    Added installer for phpcs

---
+++ build/install_phpcs.sh
@@ -0,0 +1,4 @@
+curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
+mv phpcs.phar /usr/bin/phpcs
+curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
+mv phpcbf.phar /usr/bin/phpcbf
