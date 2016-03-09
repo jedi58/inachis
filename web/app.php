38f3ede9723977f19816e2950fc4e911ccb21bbe
David G. Paul <jedi58@users.noreply.github.com>
2016-03-09 08:53:36 +0000

    Update app.php

--- web/app.php
+++ web/app.php
@@ -2,6 +2,8 @@
 
 $loader = require __DIR__ . '/../app/autoload.php';
 
+// add code for handling requests and responses - this should utilise the router
+
 //$request = Request::createFromGlobals();
 //$response = $kernel->handle($request);
 //$response->send();
