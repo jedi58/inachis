346c5b439874d82522a25630341efcf2af4d6c96
David G. Paul <jedi58@users.noreply.github.com>
2016-03-09 08:02:16 +0000

    Create app.php
    The start of the front-end application

---
+++ web/app.php
@@ -0,0 +1,8 @@
+<?php
+
+$loader = require __DIR__ . '/../app/autoload.php';
+
+//$request = Request::createFromGlobals();
+//$response = $kernel->handle($request);
+//$response->send();
+//$kernel->terminate($request, $response);
