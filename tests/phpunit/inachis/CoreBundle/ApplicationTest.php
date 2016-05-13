727690da104ac1bdab52007672a86c623744d592
David G. Paul <jedi58@gmail.com>
2016-05-13 08:30:03 +0100

    Added unit test for `Application`

---
+++ tests/phpunit/inachis/CoreBundle/ApplicationTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Inachis\Tests\CoreBundle;
+
+use Inachis\Component\CoreBundle\Application;
+
+/**
+ * @Entity
+ * @group unit
+ */
+class ApplicationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetEnvDev()
+    {
+        $this->assertEquals(
+            'dev',
+            Application::getInstance('dev')->getEnv()
+        );
+    }
+    public function testGetEnvProd()
+    {
+        Application::getInstance()->setEnv('prod');
+        $this->assertEquals(
+            'prod',
+            Application::getInstance()->getEnv()
+        );
+    }
+    public function testGetRouter()
+    {
+        $this->assertInstanceOf(
+            'Inachis\Component\CoreBundle\Routing\RoutingManager',
+            Application::getInstance()->getRouter()
+        );
+    }
+    public function testGetConfig()
+    {
+        $this->assertArrayHasKey('system.json', Application::getInstance()->getConfig());
+    }
+    public function testGetApplicationRoot()
+    {
+        $this->assertEquals(
+            str_replace('tests/phpunit/inachis/CoreBundle', '', __DIR__),
+            Application::getInstance()->getApplicationRoot()
+        );
+    }
+    public function testGetServiceExists()
+    {
+        Application::getInstance()->addService('test', 'a test function');
+        $this->assertEquals('a test function', Application::getInstance()->getService('test'));
+    }
+    public function testGetServiceDoesNotExist()
+    {
+        $this->assertEquals(null, Application::getInstance()->getService('test2'));
+    }
+}
