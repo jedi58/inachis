b74985d4f42f5689553af1a2247fba14475e2730
jedi58 <jedi58@gmail.com>
2016-07-04 01:38:30 -0400

    Applied fixes from StyleCI

--- tests/phpunit/Inachis/CoreBundle/Security/EncryptionTest.php
+++ tests/phpunit/Inachis/CoreBundle/Security/EncryptionTest.php
@@ -11,8 +11,8 @@ use ParagonIE\Halite\Symmetric\EncryptionKey;
  */
 class EncryptionTest extends \PHPUnit_Framework_TestCase
 {
-	public $encryptor;
-	public $key = '8b0ffdf9f435daa396016ea99df4136a69309fe68d1d61a32d063ca8425739a56ba97d3dd563dc3eacf84084c31e57e7';
+    public $encryptor;
+    public $key = '8b0ffdf9f435daa396016ea99df4136a69309fe68d1d61a32d063ca8425739a56ba97d3dd563dc3eacf84084c31e57e7';
 
     public function setUp()
     {
@@ -30,12 +30,12 @@ class EncryptionTest extends \PHPUnit_Framework_TestCase
     public function testDecrypt()
     {
         $this->assertEquals(
-        	'this is a test',
+            'this is a test',
             $this->encryptor->decrypt(
-        		'3142010058d3d926e6f610982dcddc8d151c7de1ee4c2aad1a52090bd43e863a52f5b80fb1387be330100a40f3db18' . 
-            	'7f8ac99eba5b200965101573f8bad11fc07a62f022a25ee2c57adeb9c9e1d7d770ed25fa1752d682b29d6e2b10cf53' . 
-            	'd3aca337de4f3637f396e745'
-        	)
+                '3142010058d3d926e6f610982dcddc8d151c7de1ee4c2aad1a52090bd43e863a52f5b80fb1387be330100a40f3db18' .
+                '7f8ac99eba5b200965101573f8bad11fc07a62f022a25ee2c57adeb9c9e1d7d770ed25fa1752d682b29d6e2b10cf53' .
+                'd3aca337de4f3637f396e745'
+            )
         );
     }
 }
