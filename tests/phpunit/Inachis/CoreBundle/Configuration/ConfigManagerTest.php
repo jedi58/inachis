45ae1fbdfe8379df52a109a10e9c656e041cec51
Dave Paul <dave.paul@jadu.net>
2016-07-12 15:33:06 +0100

    Fixed unit tests

---
+++ tests/phpunit/Inachis/CoreBundle/Configuration/ConfigManagerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Inachis\Tests\CoreBundle;
+
+use Inachis\Component\CoreBundle\Configuration\ConfigManager;
+
+/**
+ * @Entity
+ * @group unit
+ */
+class ConfigManagerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLoad()
+    {
+        $config = ConfigManager::getInstance()->load('core', 'json', 'routing/');
+        $this->assertEquals(true, is_array($config));
+    }
+
+    public function testLoadAll()
+    {
+        $config = ConfigManager::getInstance()->loadAll();
+        $this->assertEquals(true, is_array($config));
+    }
+
+    public function testLoadAllFromLocation()
+    {
+        $config = ConfigManager::getInstance()->loadAllFromLocation('');
+        $this->assertEquals(true, is_array($config));
+    }
+}
---
+++ tests/phpunit/Inachis/CoreBundle/Routing/RouteTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Inachis\Tests\CoreBundle\Routing;
+
+use Inachis\Component\CoreBundle\Routing\Route;
+use Inachis\Component\CoreBundle\Exception\RouteConfigException;
+
+/**
+ * @Entity
+ * @group unit
+ */
+class RouteTest extends \PHPUnit_Framework_TestCase
+{
+    public $route;
+
+    public function setUp()
+    {
+        $this->route = new Route();
+    }
+
+    public function testHydrateException()
+    {
+        $this->setUp();
+        try {
+            $this->route->hydrate(null);
+        } catch (RouteConfigException $exception) {
+            $this->assertContains('Route could not be parsed', $exception->getMessage());
+        }
+    }
+
+    public function testGetMethodsDefault()
+    {
+        $this->route = new Route(array());
+        $this->assertEquals(array('GET'), $this->route->getMethods());
+    }
+
+    public function testSetMethodsDefault()
+    {
+        $this->setUp();
+        $this->route->setDefaultMethods();
+        $this->assertEquals(array('GET'), $this->route->getMethods());
+    }
+
+    public function testSetMethodsEmpty()
+    {
+        $this->setUp();
+        try {
+            $this->route->setMethods(array());
+        } catch (RouteConfigException $exception) {
+            $this->assertContains('HTTP/1.1', $exception->getMessage());
+        }
+    }
+
+    public function testSetActionEmpty()
+    {
+        $this->setUp();
+        try {
+            $this->route->setAction('test');
+        } catch (RouteConfigException $exception) {
+            $this->assertContains('Invalid function name for route', $exception->getMessage());
+        }
+    }
+
+    public function testSetPathEmpty()
+    {
+        $this->setUp();
+        try {
+            $this->route->setPath('');
+        } catch (RouteConfigException $exception) {
+            $this->assertContains('Path for routing cannot be empty', $exception->getMessage());
+        }
+    }
+
+    public function testFormatRoute()
+    {
+        $this->setUp();
+        $this->route->setMethods(array('GET', 'POST'));
+        $this->route->setPath('/test');
+        $this->route->setAction('Inachis\Tests\CoreBundle\Routing\RouteTest::testFormatRoute');
+        $this->assertEquals(
+            'Methods: GET,POST' . PHP_EOL .
+            'Path:    /test' . PHP_EOL .
+            'Action:  Inachis\Tests\CoreBundle\Routing\RouteTest::testFormatRoute' . PHP_EOL,
+            $this->route->formatRoute()
+        );
+    }
+}
