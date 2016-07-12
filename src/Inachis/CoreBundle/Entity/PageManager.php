71595f5c781ef5fc16a6951e6c85734d3b5d8e2d
David G. Paul <jedi58@gmail.com>
2016-07-12 14:03:54 +0100

    Added count functions for published/draft posts/pages

--- src/Inachis/CoreBundle/Entity/PageManager.php
+++ src/Inachis/CoreBundle/Entity/PageManager.php
@@ -34,4 +34,26 @@ class PageManager extends AbstractManager
         $this->em->remove($page);
         $this->em->flush();
     }
+
+    public function getDraftCount()
+    {
+        $qb = $this->getRepository()->createQueryBuilder('q');
+        return (int) $qb
+            ->select('count(q.id)')
+            ->where('q.status = :status')
+            ->setParameter('status', Page::DRAFT)
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+
+    public function getPublishedCount()
+    {
+        $qb = $this->getRepository()->createQueryBuilder('q');
+        return (int) $qb
+            ->select('count(q.id)')
+            ->where('q.status = :status')
+            ->setParameter('status', Page::PUBLISHED)
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
 }
