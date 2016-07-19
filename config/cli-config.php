816a53542a567771b558ee65e712c2a8bf00244e
David G. Paul <jedi58@gmail.com>
2016-07-19 11:47:27 +0100

    Removed unused includes

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,8 +1,6 @@
 <?php
 use Inachis\Component\CoreBundle\Application;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
