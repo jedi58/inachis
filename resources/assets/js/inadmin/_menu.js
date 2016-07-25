223a16fd11194354c711db2258464f8af79735fd
David G. Paul <jedi58@gmail.com>
2016-07-25 19:17:02 +0100

    Added admin UI javascript for menu The menu bar is now powered by a Javascript object that has functions for controlling various events.

---
+++ resources/assets/js/inadmin/_menu.js
@@ -0,0 +1,33 @@
+var InachisNavMenu = {
+	_navNewVisible: false,
+
+	_init: function()
+	{
+		$('.admin__add-content').click($.proxy(function() {
+			$('.admin__nav-new').toggle();
+			this._navNewVisible = !this._navNewVisible;
+			Inachis._log('New menu visible: ' + this._navNewVisible);
+			if (this._navNewVisible) {
+				$(document).mouseup($.proxy(this.newNavMouseOut, this));
+			}
+		}, this));
+		$('.admin__nav-expand a, .admin__nav-collapse a').click(function () {
+			$('.admin__container').toggleClass('admin__container--collapsed admin__container--expanded');
+		});
+	},
+
+	newNavMouseOut: function(e)
+	{
+		var container = $('.admin__nav-new');
+		if (!container.is(e.target) && container.has(e.target).length === 0)  {
+			container.hide();
+			this._navNewVisible = !this._navNewVisible;
+			Inachis._log('New menu visible: ' + this._navNewVisible);
+			$(document).unbind('mouseup', InachisNavMenu.newNavMouseOut);
+		}
+	}
+};
+
+$(document).ready(function () {
+	InachisNavMenu._init();
+});
