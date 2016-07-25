40c556cfd4462a1edfbc6faa56dddbcdd0db312d
David G. Paul <jedi58@gmail.com>
2016-07-25 19:37:48 +0100

    Changes to PageController to date

--- src/Inachis/CoreBundle/Controller/PageController.php
+++ src/Inachis/CoreBundle/Controller/PageController.php
@@ -37,7 +37,123 @@ class PageController extends AbstractController
 
     public static function getPostAdmin($request, $response, $service, $app)
     {
-        $response->body('Blog post admin controller');
+        if (!Application::getInstance()->requireAuthenticationService()->isAuthenticated()) {
+            $response->redirect('/inadmin/signin')->send();
+        }
+        $urlManager = new UrlManager(Application::getInstance()->getService('em'));
+        // Confirm URL is for existing content otherwise redirect
+        $url = $urlManager->getByUrl($request->server()->get('REQUEST_URI'));
+        if (empty($url) && 0 === preg_match(
+            '/\/?inadmin\/(post|page)\/new\/?/',
+            $request->server()->get('REQUEST_URI')
+        )) {
+            $response->redirect(sprintf(
+                '/inadmin/%s/new',
+                1 === preg_match(
+                    '/\/?[0-9]{4}\/[0-9]{2}\/[0-9]{2}\/.*/',
+                    $request->server()->get('REQUEST_URI')
+                ) ? 'post' : 'page'
+            ))->send();
+        }
+        $pageManager = new PageManager(Application::getInstance()->getService('em'));
+        $properties = $request->paramsPost()->all();
+        if (!empty($url) & !empty($properties)) {
+//            $post = $pageManager->hydrate($pageManager->getById($url->getContentId()), $properties);
+        }
+        if (empty($post) || empty($post->getId())) {
+            $post = $pageManager->create($properties);
+        }
+
+        $categoryManager = new CategoryManager(Application::getInstance()->getService('em'));
+        $tagManager = new TagManager(Application::getInstance()->getService('em'));
+        // Get default URL as this is the "live" one
+        //$url = $urlManager->getDefaultUrlByContentTypeAndId('Page', $post->getId());
+        // get all categories and tags
+        if ($request->method() == 'POST') {
+            $post->setAuthor(
+                Application::getInstance()->getService('auth')->getUserManager()->getById(
+                    Application::getInstance()->getService('session')->get('user')->getId()
+                )
+            );
+            if (empty(self::$errors)) {
+                // save URL, tags and category
+                $url = $urlManager->create(array(
+                    'content' => $post,
+                    'link' => $post->getPostDateAsLink() . $urlManager->urlify($post->getTitle())
+                ));
+                $urlManager->save($url);
+                $pageManager->save($post);
+                Application::getInstance()->getService('em')->flush();
+var_dump($post);
+exit;
+
+                //$pageManager->save($post);
+                //$respone->redirect();
+            }
+        }
+        $data = array(
+            'post' => $post,
+            'form' => (new FormBuilder(array(
+                'action' => $request->server()->get('REQUEST_URI'),
+                'cssClasses' => 'form form__post'
+            )))
+            ->addComponent(new FieldsetType(array(
+                'legend' => 'Edit Post'
+            )))
+            ->addComponent(new TextType(array(
+                'name' => 'title',
+                'autofocus' => true,
+                'cssClasses' => 'field__wide',
+                'label' => 'Post Title',
+                'placeholder' => 'e.g. My first blog post',
+                'required' => true,
+                'value' => $post->getTitle()
+            )))
+            ->addComponent(new TextType(array(
+                'name' => 'subTitle',
+                'cssClasses' => 'field__wide',
+                'label' => 'Sub-title',
+                'value' => $post->getSubTitle()
+            )))
+            ->addComponent(new TextAreaType(array(
+                'name' => 'content',
+                'value' => $post->getContent()
+            )))
+            ->addComponent(new TextType(array(
+                'name' => 'link',
+                'cssClasses' => 'field__wide',
+                'label' => 'URL',
+                'required' => true,
+                'value' => $post->getUrls()
+            )))
+            ->addComponent(new SelectType(array(
+                'name' => 'tags',
+                'cssClasses' => 'field__wide js-select',
+                'label' => 'Tags',
+                //'value' => $post->getTags()
+            )))
+            ->addComponent(new NumberType(array(
+                'name' => 'postDate',
+                'cssClasses' => '',
+                'label' =>'Publication Date',
+                'type' => 'datetime'
+            )))
+            ->addComponent(new ButtonType(array(
+                'label' => !empty($post->getId()) ? 'Update' : 'Save',
+                'cssClasses' => 'button button--positive',
+                'type' => 'submit'
+            ))),
+            //featureImage
+            //featureSnippet
+            //status
+            //visibility
+            //postDate
+            //password
+            //allowComments
+            'includeEditor' => true,
+            'session' => $_SESSION
+        );
+        $response->body($app->twig->render('admin__post__edit.html.twig', $data));
     }
     public static function getPage($request, $response, $service, $app)
     {
