1b226ffbc929382c42c911a5f037218330369335
David G. Paul <jedi58@gmail.com>
2016-07-27 07:59:17 +0100

    Content tabs now work in the dashboard Implemented specifically for the dashboard - a future update will make this generic so that the code can be reused for tabbed content elsewhere within the admin interface.

--- resources/assets/js/inadmin/_dashboard.js
+++ resources/assets/js/inadmin/_dashboard.js
@@ -1,10 +1,18 @@
 var InachisDashboard = {
+	activeTab: null,
 
 	_init: function()
 	{
 		var tabs = $('.widget-posts__tabs a').click($.proxy(function (e) {
 			Inachis._log('Posts tab: ' + e.target.dataset.target);
+			$(this.activeTab.parentNode).toggleClass('widget-posts__tabs__active');
+			$(e.target.parentNode).toggleClass('widget-posts__tabs__active');
+			Inachis._log('Hiding tab: ' + this.activeTab.dataset.target);
+			Inachis._log('Showing tab: ' + e.target.dataset.target);
+			$('.' + this.activeTab.dataset.target + ',' + '.' + e.target.dataset.target).toggleClass('widget-posts__active');
+			this.activeTab = e.target;
 		}, this));
+		this.activeTab = tabs[0];
 	}
 };
 
