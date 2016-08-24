1689bc52f69087477f94042feb086fb48e499a3c
David G. Paul <jedi58@gmail.com>
2016-08-24 15:53:21 +0100

    Added JS for datetimepicker, select2, and switch components

--- resources/assets/js/inadmin/_selects.js
+++ resources/assets/js/inadmin/_selects.js
@@ -1,6 +1,79 @@
 $(document).ready(function() {
-	$('.js-select').select2({
-		// fill in options here based on data attributes
-		// see https://select2.github.io/examples.html
-	})
+	// https://select2.github.io/examples.html
+	$('.js-select').each(function ()
+	{
+		$properties = {
+
+		};
+		if ($(this).attr('data-tags')) {
+			$properties.tags = 'true';
+			$properties.tokenSeparators = [ ',' ];
+		}
+		if ($(this).attr('data-url')) {
+			$properties.ajax = {
+				url: $(this).attr('data-url'),
+				dataType: 'json',
+				data: function (params)
+				{
+					return {
+						q: params.term, // search term
+						page: params.page
+					};
+				},
+				processResults: function (data, params)
+				{
+					// parse the results into the format expected by Select2
+					// since we are using custom formatting functions we do not need to
+					// alter the remote JSON data, except to indicate that infinite
+					// scrolling can be used
+					params.page = params.page || 1;
+
+					return {
+						results: data.items,
+						pagination: {
+							more: (params.page * 30) < data.total_count
+						}
+					};
+				},
+				cache: true
+			};
+		}
+		$(this).select2($properties);
+	});
+
+	// https://github.com/daredevel/jquery-tree
+	$('ui-tree').each(function()
+	{
+		
+	});
+
+	// http://xdsoft.net/jqplugins/datetimepicker/
+	if ($('html').attr('lang')) {
+		$.datetimepicker.setLocale($('html').attr('lang'));
+	}
+	$('#publishDate').each(function ()
+	{
+		$(this).datetimepicker({
+			format: 'Y/m/d H:i',
+			validateOnBlue: false
+		});
+	});
+
+	$('.ui-switch').each(function ()
+	{
+		$properties = {
+			checked: this.checked,
+			clear: true
+		};
+		if ($(this).attr('data-labelOn')) {
+			$properties.on_label = $(this).attr('data-labelOn');
+		}
+		if ($(this).attr('data-labelOff')) {
+			$properties.off_label = $(this).attr('data-labelOff');
+		}
+		$(this).switchButton($properties);
+	});
+
+
+	$('.ui-tabbed').tabs();
 });
