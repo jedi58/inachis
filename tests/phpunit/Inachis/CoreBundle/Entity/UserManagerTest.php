188a1960e96a4bdaef63c337cc952abffb7a0742
David G. Paul <jedi58@gmail.com>
2016-09-09 13:44:43 +0100

    Removed unrequired test

--- tests/phpunit/Inachis/CoreBundle/Entity/UserManagerTest.php
+++ tests/phpunit/Inachis/CoreBundle/Entity/UserManagerTest.php
@@ -58,14 +58,6 @@ class UserManagerTest extends \PHPUnit_Framework_TestCase
             ->andReturn($users);
         $this->assertSame($users, $this->manager->getAll(10, 0));
     }
-    
-    public function testGetById()
-    {
-        $this->user = $this->manager->create($this->properties);
-        $this->repository->shouldReceive('find')->with(1)
-            ->andReturn($this->user);
-        $this->assertSame($this->user, $this->manager->getById(1));
-    }
 
     public function testSave()
     {
