4bb10408208431686a4d0cb16327fb7e3ac7941e
David G. Paul <jedi58@gmail.com>
2018-07-05 09:26:05 +0100

    Updated `UrlNoramliser` test cases

--- tests/phpunit/Utils/UrlNormaliserTest.php
+++ tests/phpunit/Utils/UrlNormaliserTest.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: davep
- * Date: 20/06/2018
- * Time: 15:47
- */
 
 namespace App\Tests\phpunit\Utils;
 
@@ -16,14 +10,22 @@ class UrlNormaliserTest extends TestCase
     public function testFromUri()
     {
         $this->assertEquals('some-short-text', UrlNormaliser::toUri('some short text'));
+        $this->assertEquals('some-short-text', UrlNormaliser::toUri('Some Short Text'));
         $this->assertEquals('some-short-text', UrlNormaliser::toUri('some  short  text'));
         $this->assertEquals('some-short-text', UrlNormaliser::toUri('some short text'));
         $this->assertEquals('s0me-short-text', UrlNormaliser::toUri('s0me short text'));
         $this->assertEquals('some-short-text', UrlNormaliser::toUri('some short text $%'));
         $this->assertEquals('some-short-text', UrlNormaliser::toUri('$ some short text'));
         $this->assertEquals('some-short-text', UrlNormaliser::toUri('some_short text'));
+        $this->assertEquals('some-hort-text', UrlNormaliser::toUri('some $hort text'));
         $this->assertEquals('some', UrlNormaliser::toUri('some short text', 5));
         $this->assertEquals('some-s', UrlNormaliser::toUri('some short text', 6));
+    }
+
+    public function testFromUriException()
+    {
+        $this->expectException(\TypeError::class);
+        UrlNormaliser::toUri(['string-in-an-array']);
     }
 
     public function testToUri()
