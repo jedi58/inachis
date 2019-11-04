402e7e8d316d97e1cb94cad07769d2ff51b0b503
David G. Paul <jedi58@gmail.com>
2019-11-04 10:11:04 +0000

    Fixes issue with importing multiple MD files concurrently

--- src/Controller/ImportController.php
+++ src/Controller/ImportController.php
@@ -33,12 +33,13 @@ class ImportController extends AbstractInachisController
     {
         $form = $this->createFormBuilder()->getForm();
         $form->handleRequest($request);
-        foreach ($request->files as $file) {
-            if ($file[0]->getError() != UPLOAD_ERR_OK) {
+
+        foreach ($request->files->get('markdownFiles') as $file) {
+            if ($file->getError() != UPLOAD_ERR_OK) {
                 return $this->json('error', 400);
             }
             $parser = new MarkdownFileParser();
-            $post = $parser->parse($this->getDoctrine()->getManager(), file_get_contents($file[0]->getRealPath()));
+            $post = $parser->parse($this->getDoctrine()->getManager(), file_get_contents($file->getRealPath()));
             if ($post->getTitle() !== '' && $post->getContent() !== '') {
                 $newLink = $post->getPostDateAsLink() . '/' .
                     UrlNormaliser::toUri(
